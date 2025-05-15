8a01f2dd5149f54f3d6b9081a9f836232665a1b6
April200907 <manaloapril2009@gmail.com>
2025-05-15 20:05:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 global.commands = new Map();
 global.events = new Map();
+global.botID = api.getCurrentUserID();
 
 const log = {
     info: (msg) => console.log(chalk.blue("[INFO]"), msg),
